3fe425a8df6b15aadb5329080276ae3b34e96613
show.ys <show.ys@antfin.com>
2022-08-26 17:49:41 +0800

    change file-explore from neo-tree to nvim-tree

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -100,14 +100,20 @@ return require('packer').startup(function()
     end
   } 
   -- file explorer
+  -- use ({
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v2.x",
+  --   requires = { 
+  --     "nvim-lua/plenary.nvim",
+  --     --"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --   }
+  -- })
   use ({
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      --"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
+    'kyazdani42/nvim-tree.lua',
+    -- requires = {
+    --   'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    -- },
   })
   use "nvim-telescope/telescope.nvim"
 end)
